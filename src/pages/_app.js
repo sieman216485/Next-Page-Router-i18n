eb35b19912891e66914aba28011f39cc9e5c7126
sieman216485 <119700542+sieman216485@users.noreply.github.com>
2023-12-15 08:15:18 -0500

    feat: add multi-language feature

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,9 @@
 import '@/styles/globals.css'
 
-export default function App({ Component, pageProps }) {
+import { appWithTranslation } from 'next-i18next'
+
+function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+
+export default appWithTranslation(MyApp)
---
+++ src/pages/other.js
@@ -0,0 +1,26 @@
+import { useTranslation } from 'next-i18next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+
+export async function getStaticProps({ locale }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, [
+        'common',
+      ])),
+      // Will be passed to the page component as props
+    },
+  }
+}
+
+export default function Home() {
+  const { t } = useTranslation('common')
+
+  return (
+    <>
+      <h1>Other Page</h1>
+      <p>
+        {t('hello')}
+      </p>
+    </>
+  )
+}
